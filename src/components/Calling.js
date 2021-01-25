22c490df79d30ebc6e1262dd55fed6fc455d3bd4
gudegege <gudegege13@gmail.com>
2021-01-25 15:05:10 +0900

    発信制御追加

--- src/components/Calling.js
+++ src/components/Calling.js
@@ -1,6 +1,6 @@
 export function Calling() {
   return (
-    <p><b className="has-text-danger">Calling......</b>
+    <p><b className="has-text-danger">呼び出し中</b><br />
       <img
         className="is_rounded"
         src="/images/Preloader_10.gif"
--- src/pages/Root.js
+++ src/pages/Root.js
@@ -3,9 +3,22 @@ import {useState } from "react";
 import Peer from 'skyway-js';
 const peer = new Peer({key: process.env.REACT_APP_SKYWAY_KEY, debug: 3});
 
+function Calling() {
+  return (
+    <p><b className="has-text-danger">呼び出し中</b><br />
+      <img
+        className="is_rounded"
+        src="/images/Preloader_10.gif"
+        alt="preloader"
+      />
+    </p>
+  );
+}
 export function RootPage() {
   const [myId, setMyId] = useState('')
   const [peers, setPeers] = useState([])
+  const [call_stat, setCallStat] = useState(0)
+  const [caller_stat, setCallerStat] = useState(0)
   let localStream;
 
   // カメラ映像取得
@@ -33,6 +46,14 @@ export function RootPage() {
   const makeCall = () => {
     const theirID = document.getElementById('their-id').value;
     console.log(theirID)
+    setCallerStat(1)
+    setTimeout(() => {
+      console.log('イド');
+      mediaConnection.answer(localStream);
+      setEventListener(mediaConnection);
+      setCallStat(2)
+      setCallerStat(2)
+    }, 5000);
     const mediaConnection = peer.call(theirID, localStream);
     setEventListener(mediaConnection);
   };
@@ -48,9 +69,16 @@ export function RootPage() {
 
   //着信処理
   peer.on('call', mediaConnection => {
-    mediaConnection.answer(localStream);
-    setEventListener(mediaConnection);
+    setCallStat(1)
+    setTimeout(() => {
+      console.log('イド');
+      mediaConnection.answer(localStream);
+      setEventListener(mediaConnection);
+      setCallStat(2)
+      setCallerStat(2)
+    }, 5000);
   });
+
   return (
     <>
       <h2 className="title is-3">テスト</h2>
@@ -76,7 +104,8 @@ export function RootPage() {
                 </select>
                 <button onClick={makeCall}>発信</button>
                 <p id="my-id"></p>
-                <video id="my-video" width="400px" autoPlay muted playsInline></video>
+                { caller_stat == 1 ? <Calling /> : "" }
+                <video id="my-video" width="200px" autoPlay muted playsInline style={{display: caller_stat > 1 ? 'block' : 'none' }} ></video>
               </h3>
             </div>
           </div>
@@ -98,7 +127,8 @@ export function RootPage() {
                 <span className={"tag is-danger"}>
                   状態: ON
                 </span><br />
-                <video id="their-video" width="400px" autoPlay playsInline></video>
+                { call_stat == 1 ? <Calling /> : "" }
+                <video id="their-video" width="300px" autoPlay playsInline style={{display: call_stat > 1 ? 'block' : 'none' }}></video>
               </h3>
             </div>
           </div>
